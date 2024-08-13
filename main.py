78b16bce757af977e704bb36e76972071aab273b
e13q <110967581+e13q@users.noreply.github.com>
2024-08-13 23:05:31 +0300

    download books from tululu.org with content check.

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ import os
 SUB_DIR_NAME = 'Books'
 
 
-def save_book(book):
-    with open(f"./{SUB_DIR_NAME}/{id}.txt", "wb") as f:
+def check_for_redirect(response):
+    if len(response.history) > 0:
+        raise requests.exceptions.TooManyRedirects
+
+
+def save_book(book, book_name):
+    if not os.path.exists(SUB_DIR_NAME):
+        os.makedirs(SUB_DIR_NAME)
+    with open(f"./{SUB_DIR_NAME}/{book_name}.txt", "wb") as f:
         f.write(book)
 
 
@@ -17,23 +24,20 @@ def get_book(book_id):
         url,
         params={
             'id': book_id
-        },
-        allow_redirects=False
+        }
     )
     response.raise_for_status()
-    if response.status_code == 302:
-        raise requests.exceptions.TooManyRedirects
+    check_for_redirect(response)
     return response.content
 
 
 if __name__ == '__main__':
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    if not os.path.exists(SUB_DIR_NAME):
-        os.makedirs(SUB_DIR_NAME)
-    try:
-        for book_id in range(1, 11):
-            get_book(book_id)
-    except (requests.exceptions.HTTPError):
-        print(f'HTTPerror {requests.exceptions.HTTPError.response.text}')
-    except (requests.exceptions.TooManyRedirects):
-        print('Website page has been moved')
+    for book_id in range(1, 11):
+        try:
+            book = get_book(book_id)
+            save_book(book, book_id)
+        except (requests.exceptions.HTTPError):
+            print(f'HTTPerror {requests.exceptions.HTTPError.response.text}')
+        except (requests.exceptions.TooManyRedirects):
+            print(f'Website page for book id {book_id} has been moved')
